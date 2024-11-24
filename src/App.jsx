859ac9f763deffe57acbbb462fc6608a97834634
seharsitara <seharsitara12@gmail.com>
2024-11-24 17:55:36 +0500

    add a responsive footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div >
+    <div className="flex flex-col justify-between h-screen">
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer className="shadow dark:bg-gray-900 mt-4 w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      
+        <span className="block text-sm text-gray-500 sm:text-center md:text-center text-center dark:text-gray-400">© 2024 Copyright. All Rights Reserved.</span>
+
+</footer>
+    </div>
+  )
+}
+
+export default Footer
